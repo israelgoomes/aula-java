58e9c64741dbcec44faf37b4dc8d7750e2416eef
israelgoomes <rael_goomes@hotmail.com>
2021-03-19 21:07:07 -0300

    Correção exercicios simulado

---
+++ Exercicios2/src/exercicios2/Exercicios2.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Israel Gomes
+ */
+public class Exercicios2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//       Construa um código em Java que imprima a soma de todos os números ímpares entre 1 e 100 na tela.
+
+//        int soma = 0;
+//
+//        for(int i = 1; i <= 100; i++){
+//            if(i % 2 != 0){
+//                soma += i;
+//            }
+//        }
+//        
+//        System.out.println("Resultado :" + soma);
+        
+        
+//        Construa um código em Java utilizando a instrução switch case, 
+//        que leia o código de um determinado produto e mostre sua classificação. Utilize a seguinte tabela como referência:
+    
+          Scanner ler = new Scanner(System.in);
+          
+//          int num;
+//          
+//          System.out.println("Digite um número de 1 a 15: ");
+//          num = ler.nextInt();
+//          
+//          switch(num){
+//              case 1:
+//                  System.out.println("Alimento não-perecível");
+//                  break;
+//              case 2:
+//                  System.out.println("Alimento perecível");
+//                  break;
+//              case 3:
+//                  System.out.println("Alimento perecível");
+//                  break;
+//              case 4:
+//                  System.out.println("Alimento perecível");
+//                  break;
+//              case 5:
+//                  System.out.println("Vestuário");
+//                  break;
+//              case 6:
+//                  System.out.println("Vestuário");
+//                  break;
+//              case 7:
+//                  System.out.println("Higiene Pessoal");
+//                  break;
+//              case 8:
+//                  System.out.println("Limpeza de Utensílios Domésticos");
+//                  break;
+//              case 9:
+//                  System.out.println("Limpeza de Utensílios Domésticos");
+//                  break;
+//              case 10:
+//                  System.out.println("Limpeza de Utensílios Domésticos");
+//                  break;
+//              case 11:
+//                  System.out.println("Limpeza de Utensílios Domésticos");
+//                  break;
+//              case 12:
+//                  System.out.println("Limpeza de Utensílios Domésticos");
+//                  break;
+//              case 13:
+//                  System.out.println("Limpeza de Utensílios Domésticos");
+//                  break;
+//              case 14:
+//                  System.out.println("Limpeza de Utensílios Domésticos");
+//                  break;
+//              case 15:
+//                  System.out.println("Limpeza de Utensílios Domésticos");
+//                  break;  
+//              default: 
+//                  System.out.println("Código innválido");
+//          }
+
+//Construa um código em Java que imprima na tela a tabuada do 6. Utilize a instrução while. A saída de dados deve ser assim:
+//	
+//1 x 6 = 6
+//2 x 6 = 12....
+        
+
+//    int count = 1;
+//    
+//    while(count <= 10){
+//        System.out.println(count + " X " + "6" + " = " + count*6);
+//        count++;
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int a = 5;
+int b = 5;
+
+        System.out.println(++b);
+        System.out.println(a++);
+        System.out.println(a);
+
+    }
+    
+}
